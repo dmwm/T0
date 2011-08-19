3149cdafc5aba5ec2d69bc255a6586f42aa399ee
hufnagel <hufnagel@4525493e-7705-40b1-a816-d608a930855b>
2011-08-19 15:39:04 +0000

    Changes binds on InsertLumi DAO #2150
    Signed-off-by: Dirk Hufnagel <Dirk.Hufnagel@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/T0/trunk@13587 4525493e-7705-40b1-a816-d608a930855b

--- src/python/T0/WMBS/Oracle/InsertSplitLumis.py
+++ src/python/T0/WMBS/Oracle/InsertSplitLumis.py
@@ -9,14 +9,13 @@ from WMCore.Database.DBFormatter import DBFormatter
 
 class InsertSplitLumis(DBFormatter):
 
-    sql = """BEGIN
-               INSERT INTO lumi_section_split_active
-               (RUN_ID, LUMI_ID, STREAM_ID)
-               VALUES (:run_id, :lumi_id, :stream_id);
-             EXCEPTION
-               WHEN DUP_VAL_ON_INDEX THEN NULL;
-               WHEN OTHERS THEN RAISE;
-             END;
+    sql = """INSERT INTO lumi_section_split_active
+             (RUN_ID, LUMI_ID, STREAM_ID)
+             SELECT run_stream_sub_assoc.run_id,
+                    :lumi,
+                    run_stream_sub_assoc.stream_id
+             FROM run_stream_sub_assoc
+             WHERE run_stream_sub_assoc.subscription = :sub
              """
 
     def execute(self, binds, conn = None, transaction = False):
