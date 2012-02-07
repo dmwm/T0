f99b763209c498406baa46dc8a67122da09b85fd
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2012-02-07 19:58:33 +0000

    One more change of version, now I fixed as development and modified t0.spec to change development string to version number provided in spec file (svn tag).
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/T0/trunk@15154 4525493e-7705-40b1-a816-d608a930855b

--- src/python/T0/__init__.py
+++ src/python/T0/__init__.py
@@ -1,1 +1,5 @@
-version = '1.0'
+# The version variable assigned here can be used by setup.py file and sphinx. 
+# At the same time CMS build system is using SVN tags. Here version value is
+# a placeholder which is replaced in t0.spec during RPM build process to get
+# correct SVN tag version.
+version = 'development' # do not replace it
