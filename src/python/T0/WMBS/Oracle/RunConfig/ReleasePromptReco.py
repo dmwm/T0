2605b4eab4bf48b8d3703ba8619ebf954c1789c4
hufnagel <hufnagel@4525493e-7705-40b1-a816-d608a930855b>
2012-05-05 20:51:29 +0000

    release run for promptreco, fixes #3094
    Signed-off-by: Dirk Hufnagel <Dirk.Hufnagel@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/T0/trunk@15852 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/T0/WMBS/Oracle/RunConfig/ReleasePromptReco.py
@@ -0,0 +1,25 @@
+"""
+_ReleasePromptReco_
+
+Oracle implementation of ReleasePromptReco
+
+"""
+
+import time
+
+from WMCore.Database.DBFormatter import DBFormatter
+
+class ReleasePromptReco(DBFormatter):
+
+    def execute(self, binds, conn = None, transaction = False):
+
+        sql = """UPDATE reco_release_config
+                 SET released = :NOW
+                 WHERE run_id = :RUN
+                 AND primds_id = (SELECT id FROM primary_dataset WHERE name = :PRIMDS)
+                 """
+
+        self.dbi.processData(sql, binds, conn = conn,
+                             transaction = transaction)
+
+        return
