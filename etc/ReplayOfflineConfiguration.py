3fb0cfd8a032696fd0617099498bf0a92a3c10eb
John Casallas <johnhcasallasl@gmail.com>
2016-08-01 11:09:14 +0200

    Switch to CMSSW_8_0_16 and use of the TkAlCosmicsInCollisions AlCa produces on the NoBPTX PD

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -70,7 +70,7 @@ setPromptCalibrationConfig(tier0Config,
                            validationMode = True)
 
 # Defaults for CMSSW version
-defaultCMSSWVersion = "CMSSW_8_0_15"
+defaultCMSSWVersion = "CMSSW_8_0_16"
 
 # Configure ScramArch
 setDefaultScramArch(tier0Config, "slc6_amd64_gcc530")
@@ -114,21 +114,22 @@ repackVersionOverride = {
     }
 
 expressVersionOverride = {
-    "CMSSW_8_0_0" : "CMSSW_8_0_15",
-    "CMSSW_8_0_1" : "CMSSW_8_0_15",
-    "CMSSW_8_0_2" : "CMSSW_8_0_15",
-    "CMSSW_8_0_3" : "CMSSW_8_0_15",
-    "CMSSW_8_0_4" : "CMSSW_8_0_15",
-    "CMSSW_8_0_5" : "CMSSW_8_0_15",
-    "CMSSW_8_0_6" : "CMSSW_8_0_15",
-    "CMSSW_8_0_7" : "CMSSW_8_0_15",
-    "CMSSW_8_0_8" : "CMSSW_8_0_15",
-    "CMSSW_8_0_9" : "CMSSW_8_0_15",
-    "CMSSW_8_0_10" : "CMSSW_8_0_15",
-    "CMSSW_8_0_11" : "CMSSW_8_0_15",
-    "CMSSW_8_0_12" : "CMSSW_8_0_15",
-    "CMSSW_8_0_13" : "CMSSW_8_0_15",
-    "CMSSW_8_0_14" : "CMSSW_8_0_15"
+    "CMSSW_8_0_0" : "CMSSW_8_0_16",
+    "CMSSW_8_0_1" : "CMSSW_8_0_16",
+    "CMSSW_8_0_2" : "CMSSW_8_0_16",
+    "CMSSW_8_0_3" : "CMSSW_8_0_16",
+    "CMSSW_8_0_4" : "CMSSW_8_0_16",
+    "CMSSW_8_0_5" : "CMSSW_8_0_16",
+    "CMSSW_8_0_6" : "CMSSW_8_0_16",
+    "CMSSW_8_0_7" : "CMSSW_8_0_16",
+    "CMSSW_8_0_8" : "CMSSW_8_0_16",
+    "CMSSW_8_0_9" : "CMSSW_8_0_16",
+    "CMSSW_8_0_10" : "CMSSW_8_0_16",
+    "CMSSW_8_0_11" : "CMSSW_8_0_16",
+    "CMSSW_8_0_12" : "CMSSW_8_0_16",
+    "CMSSW_8_0_13" : "CMSSW_8_0_16",
+    "CMSSW_8_0_14" : "CMSSW_8_0_16",
+    "CMSSW_8_0_15" : "CMSSW_8_0_16"
     }
 
 #set default repack settings for bulk streams
@@ -205,6 +206,7 @@ datasets = [ "NoBPTX" ]
 for dataset in datasets:
     addDataset(tier0Config, dataset,
                do_reco = True,
+               alca_producers = [ "TkAlCosmicsInCollisions" ],
                dqm_sequences = [ "@common" ],
                physics_skims = [ "LogError", "LogErrorMonitor" ],
                scenario = ppScenario)
@@ -214,6 +216,7 @@ datasets = [ "NoBPTX_0T" ]
 for dataset in datasets:
     addDataset(tier0Config, dataset,
                do_reco = True,
+               alca_producers = [ "TkAlCosmicsInCollisions" ],
                dqm_sequences = [ "@common" ],
                physics_skims = [ "LogError", "LogErrorMonitor" ],
                scenario = ppScenarioB0T)
