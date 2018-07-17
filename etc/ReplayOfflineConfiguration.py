dc6f94302873012123eb983c8b2b5b777be5c76d
gkfthddk <gkfthddk@gmail.com>
2018-07-17 14:45:18 +0900

    Update ReplayOfflineConfiguration.py

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -1,10 +1,8 @@
 from __future__ import print_function
 """
 _OfflineConfiguration_
-
 Processing configuration for the Tier0 - Replay version
 """
-
 print("@@-------github.com/gkfthddk/T0--------@@")
 from T0.RunConfig.Tier0Config import addDataset
 from T0.RunConfig.Tier0Config import createTier0Config
