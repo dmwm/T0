60d1cfe495e95fed74679a218f1ed611cee16ae9
gkfthddk <gkfthddk@gmail.com>
2018-08-31 00:44:25 +0900

    compile and get log

---
+++ bin/message.py
@@ -0,0 +1,14 @@
+import sys
+from subprocess import Popen, PIPE ,STDOUT
+filename1=sys.argv[1]
+filename2=sys.argv[2]
+print("python {} {}".format(filename1,filename2))
+string=''
+p=Popen([sys.executable,'-u','compi.py', filename2],stdout=PIPE,stderr=STDOUT,close_fds=True,bufsize=0)
+for c in iter(lambda: p.stdout.read(1), ''):
+  string+=c
+p.stdout.close()
+rc=p.wait()
+print("...")
+if(string==''):print("compiling done without syntax error")
+else:print(string)
