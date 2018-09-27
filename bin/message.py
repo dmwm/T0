d60c788573b93ccd8d68ec2723c28da01af07ef0
gkfthddk <gkfthddk@gmail.com>
2018-09-27 19:31:33 +0900

    exit command

--- bin/message.py
+++ bin/message.py
@@ -2,6 +2,8 @@ import sys
 from subprocess import Popen, PIPE ,STDOUT
 filename1=sys.argv[1]
 filename2=sys.argv[2]
+try:button=eval(sys.argv[3])
+except:button=0
 print("python {} {}".format(filename1,filename2))
 string=''
 p=Popen([sys.executable,'-u',filename1, filename2],stdout=PIPE,stderr=STDOUT,close_fds=True,bufsize=0)
@@ -10,4 +12,8 @@ for c in iter(lambda: p.stdout.read(1), ''):
 p.stdout.close()
 rc=p.wait()
 if(string==''):print("##compiling done without syntax error : "+filename2)
-else:print(string)
+else:
+  if(button!=0):
+    sys.exit(button)
+  else:
+    print(string)
