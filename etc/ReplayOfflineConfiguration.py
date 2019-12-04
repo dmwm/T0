c5812ad4a9c4710941cf3c102108520f2244c173
gkfthddk <gkfthddk@gmail.com>
2019-12-04 21:22:51 +0900

    Update ReplayOfflineConfiguration.py

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -35,7 +35,7 @@ tier0Config = createTier0Config()
 setConfigVersion(tier0Config, "replace with real version")
 
 # Set run number to replay
-setInjectRuns(tier0Config, [ 333529 ])
+setInjectRuns(tier0Config, [ 333680 ])
 
 # Settings up sites
 processingSite = "T2_CH_CERN"
