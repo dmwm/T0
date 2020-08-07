ebb643cdeb3f6678031f45ad7a2402363fa0c2d9
CMS Tier0 <cms.t0@cern.ch>
2020-08-07 12:53:43 +0200

    Base container configuration

--- etc/ContainterConfig.sh
+++ etc/ContainterConfig.sh
@@ -1,6 +1,6 @@
 #T0_VERSION specifies which image tag to use
-T0_VERSION=2.1.9
+T0_VERSION=2.2.0
 
 #WMC_PR specifies a list of WMCore Pull Request to be tested
 #WMC_PR=""
-WMC_PR="9660 9716"
+WMC_PR=""
