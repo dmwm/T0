f528744358e59afc6aad3bf817d7e2d984a88e5a
germanfgv <gf.giraldo239@uniandes.edu.co>
2020-10-12 16:16:19 +0200

    Changes to allow single lumi Replays

--- src/python/T0/RunConfig/Tier0Config.py
+++ src/python/T0/RunConfig/Tier0Config.py
@@ -245,6 +245,7 @@ def createTier0Config():
     tier0Config.Global.InjectRuns = None
     tier0Config.Global.InjectMinRun = None
     tier0Config.Global.InjectMaxRun = None
+    tier0Config.Global.Lumi = None
 
     tier0Config.Global.ScramArches = {}
     tier0Config.Global.Backfill = None
@@ -663,6 +664,15 @@ def setInjectMaxRun(config, injectMaxRun):
     Set the highest run to be injected into the Tier0.
     """
     config.Global.InjectMaxRun = injectMaxRun
+    return
+
+def setLumi(config, lumi = 1 ):
+    """
+    _setLumi_
+
+    Set the lumi to be injected into the Tier0.
+    """
+    config.Global.Lumi = lumi
     return
 
 def setEnableUniqueWorkflowName(config):
