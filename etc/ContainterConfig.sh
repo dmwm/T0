730e2499228a1d7963a37efb914a7c156400bb9d
germanfgv <germanfgv@gmail.com>
2020-11-16 15:16:10 +0100

    Replay configuration for MWGR5

--- etc/ContainterConfig.sh
+++ etc/ContainterConfig.sh
@@ -3,11 +3,11 @@
 ###
 
 #T0_VERSION specifies which image tag to use
-T0_VERSION=2.2.0
+T0_VERSION=2.2.1
 
 #WMC_PR specifies a list of WMCore Pull Request to be tested
 #WMC_PR=""
-WMC_PR=""
+WMC_PR="10044 10067"
 
 #Specify which tests you want to run, by assigning a value of 1 to the corresponding variable
 UNIT_TEST=0
