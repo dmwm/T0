c92b1ad742e885fb5dd25f01b82cd993416fc150
Jhonatan Amado <jhonatanamadov@gmail.com>
2021-05-31 22:16:34 -0500

    py3 configuration files for Production and Replays

--- etc/ProdOfflineConfiguration.py
+++ etc/ProdOfflineConfiguration.py
@@ -3,7 +3,7 @@ _OfflineConfiguration_
 
 Processing configuration for the Tier0 - Production version
 """
-from __future__ import print_function
+
 
 from T0.RunConfig.Tier0Config import addDataset
 from T0.RunConfig.Tier0Config import createTier0Config
--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -2,7 +2,7 @@
 _OfflineConfiguration_
 Processing configuration for the Tier0 - Replay version
 """
-from __future__ import print_function
+
 
 import datetime
 from T0.RunConfig.Tier0Config import addDataset
