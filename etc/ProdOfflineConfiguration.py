20658e2ca15a21917994e8d1b5b044375f10eec2
Germán Felipe Giraldo Villa <germanfgv@gmail.com>
2021-10-25 11:05:18 +0200

    Update prod config to use CMSSW_12_0_3

--- etc/ProdOfflineConfiguration.py
+++ etc/ProdOfflineConfiguration.py
@@ -94,7 +94,7 @@ setPromptCalibrationConfig(tier0Config,
 
 # Defaults for CMSSW version
 defaultCMSSWVersion = {
-    'default': "CMSSW_12_0_2_patch2"
+    'default': "CMSSW_12_0_3"
 }
 
 # Configure ScramArch
@@ -164,6 +164,7 @@ repackVersionOverride = {
     "CMSSW_12_0_0" : defaultCMSSWVersion['default'],
     "CMSSW_12_0_1" : defaultCMSSWVersion['default'],
     "CMSSW_12_0_2" : defaultCMSSWVersion['default'],
+    "CMSSW_12_0_2_patch1" : defaultCMSSWVersion['default'],
     "CMSSW_12_0_2_patch1" : defaultCMSSWVersion['default']
     }
 
