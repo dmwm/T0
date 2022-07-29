8b81ae32872c6fe756dd01e6742898a6f2788501
Germán Felipe Giraldo Villa <germanfgv@gmail.com>
2022-07-29 09:40:27 +0200

    Add recent patches to 3.0.5 deployment

--- bin/00_patches.sh
+++ bin/00_patches.sh
@@ -15,4 +15,10 @@ DEPLOY_DIR=$BASE_DIR/srv/wmagent
 #wget -nv https://github.com/dmwm/T0/pull/4596.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
 #wget -nv https://github.com/dmwm/T0/pull/4597.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
 
-#Patches on top of 3.0.1
+#Patches on top of 3.0.5
+#Replay rdeployment ID as procesing version
+wget -nv https://github.com/dmwm/T0/pull/4705.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
+#Repack jobsplitting fix
+wget -nv https://github.com/dmwm/T0/pull/4724.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
+
+
