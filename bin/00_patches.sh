c60f9fd42c5500f6c0cf260354c36052b8ad1e68
Jhonatan Amado <jhonatanamadov@gmail.com>
2022-09-27 15:04:07 -0500

    Update patches file T0 3.0.6

--- bin/00_patches.sh
+++ bin/00_patches.sh
@@ -6,23 +6,4 @@
 BASE_DIR=/data/tier0
 DEPLOY_DIR=$BASE_DIR/srv/wmagent
 
-
-#Patches on top of 2.2.4
-
-#wget -nv https://github.com/dmwm/T0/pull/4566.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#wget -nv https://github.com/dmwm/WMCore/pull/10344.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#wget -nv https://github.com/dmwm/T0/pull/4589.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#wget -nv https://github.com/dmwm/T0/pull/4596.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#wget -nv https://github.com/dmwm/T0/pull/4597.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-
-#Patches on top of 3.0.5
-# Raise condition when run/streams can't be configured
-wget -nv https://github.com/dmwm/T0/pull/4729.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#Replay rdeployment ID as procesing version
-wget -nv https://github.com/dmwm/T0/pull/4705.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#Repack jobsplitting fix
-wget -nv https://github.com/dmwm/T0/pull/4724.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#Add AlCaPPS scnerario
-wget -nv https://github.com/dmwm/T0/pull/4718.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
-#Rucio Deletions fix
-wget -nv https://github.com/dmwm/WMCore/pull/11245.patch -O - | patch -f -d $DEPLOY_DIR/current/apps/t0/lib/python*/site-packages/ -p 3
+#Patches on top of 3.0.6
