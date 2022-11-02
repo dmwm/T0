52beeaf059c0356344ac8896d9ad683fdc85ee70
Germán Felipe Giraldo Villa <germanfgv@gmail.com>
2022-11-02 23:03:11 +0100

    Add WMCore #11353 patch
    WMCore Issue: https://github.com/dmwm/WMCore/issues/11304
    WMCore PR: https://github.com/dmwm/WMCore/pull/11353

--- bin/00_patches.sh
+++ bin/00_patches.sh
@@ -7,3 +7,7 @@ BASE_DIR=/data/tier0
 DEPLOY_DIR=$BASE_DIR/srv/wmagent
 
 #Patches on top of 3.0.6
+#Fix updates to t0_request
+#Patches on top of 3.0.6
+curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11353.patch | patch -d $DEPLOY_DIR/current/apps/t0/data/couchapps/ -p 3
+curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11353.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
