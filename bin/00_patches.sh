721fbfb138182dd58b97e0e0e73e30ceb0adec5e
Germán Felipe Giraldo Villa <germanfgv@gmail.com>
2022-11-03 15:24:14 +0100

    Add specifyStream PR to patches

--- bin/00_patches.sh
+++ bin/00_patches.sh
@@ -8,6 +8,8 @@ DEPLOY_DIR=$BASE_DIR/srv/wmagent
 
 #Patches on top of 3.0.6
 #Fix updates to t0_request
-#Patches on top of 3.0.6
 curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11353.patch | patch -d $DEPLOY_DIR/current/apps/t0/data/couchapps/ -p 3
 curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11353.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
+
+# Add SpecifyStream function
+curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4765.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
