89ccc2f64ab94ad2bb81da9e761ddf1d84bd5511
germanfgv <germanfgv@gmail.com>
2023-05-23 16:55:58 +0200

    Update InsertRecoConfig.py to support NANOAOD

--- src/python/T0/WMBS/Oracle/RunConfig/InsertRecoConfig.py
+++ src/python/T0/WMBS/Oracle/RunConfig/InsertRecoConfig.py
@@ -13,7 +13,7 @@ class InsertRecoConfig(DBFormatter):
 
         sql = """INSERT INTO reco_config
                  (RUN_ID, PRIMDS_ID, DO_RECO, RECO_SPLIT, WRITE_RECO, WRITE_DQM,
-                  WRITE_AOD, WRITE_MINIAOD, PROC_VERSION, ALCA_SKIM, PHYSICS_SKIM,
+                  WRITE_AOD, WRITE_MINIAOD, WRITE_NANOAOD, PROC_VERSION, ALCA_SKIM, PHYSICS_SKIM,
                   DQM_SEQ, CMSSW_ID, MULTICORE, SCRAM_ARCH, GLOBAL_TAG)
                  VALUES (:RUN,
                          (SELECT id FROM primary_dataset WHERE name = :PRIMDS),
@@ -23,6 +23,7 @@ class InsertRecoConfig(DBFormatter):
                          :WRITE_DQM,
                          :WRITE_AOD,
                          :WRITE_MINIAOD,
+						 :WRITE_NANOAOD,
                          :PROC_VER,
                          :ALCA_SKIM,
                          :PHYSICS_SKIM,
