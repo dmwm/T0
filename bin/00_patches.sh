b1eead5f56f98ac0daedb6365980f438f374217f
Antonio <a.linares@cern.ch>
2023-05-26 12:20:35 +0200

    Keeping patches

--- bin/00_patches.sh
+++ bin/00_patches.sh
@@ -24,5 +24,9 @@ DEPLOY_DIR=$BASE_DIR/srv/wmagent
 
 
 #Patches on top of 3.1.0
+# Adopt CMS_PATH and SITECONFIG_PATH to locate the site catalog
+curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11481.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
+# Add new CMSCouch exception for Request Entity Too Large
+curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11502.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
 # Add new pp scenario
 curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4813.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
