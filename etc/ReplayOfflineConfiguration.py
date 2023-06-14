c4c024f5ec79fb31d9bc53e587787453d63c2b2d
Andrea Piccinelli <44297061+anpicci@users.noreply.github.com>
2023-06-14 23:10:41 +0200

    Update ReplayOfflineConfiguration.py

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -102,7 +102,7 @@ setPromptCalibrationConfig(tier0Config,
 
 # Defaults for CMSSW version
 defaultCMSSWVersion = {
-    'default': "CMSSW_13_0_7"
+    'default': "CMSSW_13_0_7_TOTEM"
 }
 
 # Configure ScramArch
