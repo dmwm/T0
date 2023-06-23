96a7f0630fd768df17feac08a0e86a107ff98204
Germán Felipe Giraldo Villa <germanfgv@gmail.com>
2023-06-24 00:00:43 +0200

    Deployment scripts for 3.1.0 (#4843)

--- bin/00_patches.sh
+++ bin/00_patches.sh
@@ -6,24 +6,23 @@
 BASE_DIR=/data/tier0
 DEPLOY_DIR=$BASE_DIR/srv/wmagent
 
-#Patches on top of 3.0.7
-# Remove py2 compatibility (Needed for #11473)
-# curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11420.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
-# Deprecate TFC override for multi-step jobs
-# curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11473.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
-# Adopt CMS_PATH and SITECONFIG_PATH to locate the site catalog
-# curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11481.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
-
 #Patches on top of 3.0.8
 # Adopt CMS_PATH and SITECONFIG_PATH to locate the site catalog
-curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11481.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
+#curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11481.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
 
 # Add new CMSCouch exception for Request Entity Too Large
-curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11502.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
+#curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11502.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
 
 # Add new pp scenario
-curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4813.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
+#curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4813.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
 
+#Adding support for writing nano aod
+#curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4827.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
+
+#Adding skipped streamer info into T0 Data Service
+#curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4841.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
+
+#Patches on top of 3.1.0
 #Adding support for writing nano aod
 curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4827.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
 
