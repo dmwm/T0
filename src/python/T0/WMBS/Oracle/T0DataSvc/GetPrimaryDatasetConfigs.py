e2e4bdd4e7797f830c203e1b52dfc54eece85df9
Antonio <a.linares@cern.ch>
2023-07-28 10:58:13 -0500

    Update query in Get file

--- src/python/T0/WMBS/Oracle/T0DataSvc/GetPrimaryDatasetConfigs.py
+++ src/python/T0/WMBS/Oracle/T0DataSvc/GetPrimaryDatasetConfigs.py
@@ -14,21 +14,22 @@ class GetPrimaryDatasetConfigs(DBFormatter):
     def execute(self, conn = None, transaction = False):
 
 
-        sql = """
-                SELECT 
-                      primary_dataset_config.primds_id AS primds, 
-                      primary_dataset_config.acq_era AS acq_era, 
-                      MIN(run.run_id) AS min_run, 
-                      MAX(run.run_id) AS max_run, 
-                      primary_dataset_config.cmssw_id AS cmssw, 
-                      primary_dataset_config.global_tag AS global_tag, 
-                      primary_dataset_config.physics_skim AS physics_skim, 
-                      primary_dataset_config.dqm_seq AS dqm_seq
-                FROM primary_dataset_config
-                JOIN run ON run.run_id = primary_dataset_config.run_id
-                WHERE checkForZeroState(primary_dataset_config.in_datasvc) = 0
-                GROUP BY acq_era, primds, cmssw,  global_tag, physics_skim, dqm_seq
-                ORDER BY primds, min_run desc
+        sql = """SELECT primary_dataset.name AS primds, 
+                        run.acq_era AS acq_era, 
+                        MIN(run.run_id) AS min_run, 
+                        MAX(run.run_id) AS max_run, 
+                        cmssw_version.name AS cmssw, 
+                        reco_config.global_tag AS global_tag, 
+                        reco_config.physics_skim AS physics_skim, 
+                        reco_config.dqm_seq AS dqm_seq
+                FROM reco_config
+                INNER JOIN run ON run.run_id = reco_config.run_id
+                INNER JOIN primary_dataset ON primary_dataset.id = reco_config.primds_id
+                INNER JOIN cmssw_version ON cmssw_version.id = reco_config.cmssw_id
+                WHERE checkForZeroState(reco_config.in_datasvc) = 1
+                GROUP BY primary_dataset.name, run.acq_era, cmssw_version.name,  reco_config.global_tag, reco_config.physics_skim, reco_config.dqm_seq
+                ORDER BY primds, MIN(run.run_id) desc
+
                 """
 
         results = self.dbi.processData(sql, binds = {}, conn = conn,
