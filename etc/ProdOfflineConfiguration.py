244436e398863edbbb1a835f5f991cf178a28d2c
German Giraldo <germanfgv@gmail.com>
2023-08-29 12:53:47 +0200

    Moving to era Run2023E

--- etc/ProdOfflineConfiguration.py
+++ etc/ProdOfflineConfiguration.py
@@ -55,7 +55,7 @@ addSiteConfig(tier0Config, "T0_CH_CERN_Disk",
 #  Data type
 #  Processing site (where jobs run)
 #  PhEDEx locations
-setAcquisitionEra(tier0Config, "Run2023D")
+setAcquisitionEra(tier0Config, "Run2023E")
 setBaseRequestPriority(tier0Config, 251000)
 setBackfill(tier0Config, None)
 setBulkDataType(tier0Config, "data")
