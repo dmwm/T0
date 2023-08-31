d3a79e5cc437306b3ea9b2987b49756f154ea62f
ItyTell <89577338+ItyTell@users.noreply.github.com>
2023-08-31 17:41:28 +0300

    Update ReplayOfflineConfiguration.py

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -38,7 +38,8 @@ setConfigVersion(tier0Config, "replace with real version")
 # 367102 - Collisions 2023 - 1200b - 0.5h long - all components IN
 setInjectRuns(tier0Config, [369998])
 
-setInjectLimit(tier0Config, 10)
+# Use this in order to limit the number of lumisections to process
+#setInjectLimit(tier0Config, 10)
 
 # Settings up sites
 processingSite = "T2_CH_CERN"
