ae5b263fecf7e471d50cbfde599d2157a3b1f059
Germán Felipe Giraldo Villa <germanfgv@gmail.com>
2024-03-20 10:26:06 +0100

    Add cmsRun log fix to patches

--- bin/00_patches.sh
+++ bin/00_patches.sh
@@ -9,4 +9,5 @@ DEPLOY_DIR=$BASE_DIR/srv/wmagent
 #Emulated data handling
 curl https://patch-diff.githubusercontent.com/raw/dmwm/T0/pull/4921.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
 
-
+#Fix cmsRun logs
+curl https://patch-diff.githubusercontent.com/raw/dmwm/WMCore/pull/11933.patch | patch -d $DEPLOY_DIR/current/apps/t0/lib/python3.8/site-packages/ -p 3
