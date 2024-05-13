4521c7140c2d7b7ca61dd445c6fb1fabcb73f8b8
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2024-05-13 16:48:09 -0500

    Parallel pypi packaging and upload as WMCore (#4951)

---
+++ setup_dependencies.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+dependencies = {
+    't0': {
+        'packages': ['T0+', 
+                     'T0Component+'],
+        'modules': [],
+        'systems': [],
+        'statics': ['bin+',
+                    'etc+']
+    }
+}
