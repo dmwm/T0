56fb64352e931afb0ee302f2e423c2d8db9214bc
LinaresToine <antonio.linares.sancho@cern.ch>
2024-05-20 23:49:08 -0500

    Agent config modifications

--- bin/00_init.sh
+++ bin/00_init.sh
@@ -11,7 +11,45 @@ sleep 3
 manage clean-oracle
 
 echo "Now initializing"
+sleep 3
 bash $DEPLOY_DIR/init.sh
 
+echo "Modifying config file"
+sleep 3
+sed -i 's+TIER0_CONFIG_FILE+/data/tier0/admin/ProdOfflineConfiguration.py+' $config/config.py
+sed -i 's+TIER0_SPEC_DIR+/data/tier0/admin/Specs+' $config/config.py
+sed -i "s+config.Agent.contact = 'cms-comp-ops-workflow-team@cern.ch'+config.Agent.contact = 'cms-tier0-operations@cern.ch'+" $config/config.py
+sed -i "s+'team1,team2,cmsdataops'+'tier0production'+g" $config/config.py
 
 
+# Twiking Rucio configuration
+sed -i "s+config.RucioInjector.containerDiskRuleParams.*+config.RucioInjector.containerDiskRuleParams = {}+" $config/config.py
+sed -i "s+config.RucioInjector.metaDIDProject.*+config.RucioInjector.metaDIDProject = 'Tier0'+" $config/config.py
+
+sed -i "s+config.DBS3Upload.uploaderName = 'WMAgent'+config.DBS3Upload.uploaderName = 'T0Prod'+g" $config/config.py
+
+
+#
+# password for dropbox upload
+#
+
+DROPBOX_USER=`cat $WMA_SECRETS_FILE | grep DROPBOX_USER | sed s/DROPBOX_USER=//`
+DROPBOX_PASS=`cat $WMA_SECRETS_FILE | grep DROPBOX_PASS | sed s/DROPBOX_PASS=//`
+
+if [ "x$DROPBOX_USER" == "x" ] || [ "x$DROPBOX_PASS" == "x" ]; then
+    echo "Secrets file doesn't contain DROPBOX_USER or DROPBOX_PASS";
+    exit 1
+fi
+
+echo 'config.Tier0Feeder.dropboxuser = "'$DROPBOX_USER'"' >> $config/config.py
+echo 'config.Tier0Feeder.dropboxpass = "'$DROPBOX_PASS'"' >> $config/config.py
+
+
+#
+# Enable AgentStatusWatcher - site status automatic updated
+#
+sed -i "s+config.AgentStatusWatcher.ignoreDisks.*+config.AgentStatusWatcher.ignoreDisks = [ '/cvmfs/cvmfs-config.cern.ch', '/cvmfs/cms.cern.ch', '/eos/cms', '/cvmfs/cms-ib.cern.ch', '/cvmfs/patatrack.cern.ch' ]+" $config/config.py
+
+
+#Overwrite ErrorHandler to avoid jobs going to ensure T0 jobs can always be retried
+sed -i "s/config.ErrorHandler.maxFailTime.*/config.ErrorHandler.maxFailTime=601200/g" $config/config.py
--- etc/Tier0Config.py
+++ etc/Tier0Config.py
@@ -6,36 +6,61 @@ config.Tier0Feeder.pollInterval = 30
 config.Tier0Feeder.tier0ConfigFile = "TIER0_CONFIG_FILE"
 config.Tier0Feeder.specDirectory = "TIER0_SPEC_DIR"
 config.Tier0Feeder.requestDBName = "t0_request_local"
+config.Tier0Feeder.serviceProxy = '/data/certs/robot-proxy-vocms001.pem'
+
+config.JobAccountant.pollInterval = 30
+config.JobAccountant.maxAllowedRepackOutputSize = 24 * 1024 * 1024 * 1024
+
+config.JobArchiver.handleInjected = False
+
+config.JobCreator.pollInterval = 30
 
 config.JobSubmitter.LsfPluginQueue = "cmsrepack"
 config.JobSubmitter.LsfPluginResourceReq = "select[type==SLC5_64] rusage[pool=10000,mem=1800]"
 config.JobSubmitter.LsfPluginJobGroup = "/groups/tier0/wmagent_testing"
 config.JobSubmitter.LsfPluginBatchOutput = "None"
-
-config.JobCreator.pollInterval = 30
 config.JobSubmitter.pollInterval = 30
-config.JobAccountant.pollInterval = 30
 
 config.RetryManager.section_("PauseAlgo")
 config.RetryManager.PauseAlgo.section_("default")
-config.RetryManager.PauseAlgo.default.coolOffTime = retryAlgoParams
 config.RetryManager.PauseAlgo.default.pauseCount = 3
-config.RetryManager.plugins = {"default" : "PauseAlgo",
-                               "Cleanup" : "SquaredAlgo",
-                               "LogCollect" : "SquaredAlgo"}
+config.RetryManager.PauseAlgo.default.coolOffTime = {'create': 10, 'job': 10, 'submit': 10}
+config.RetryManager.plugins = {"default" : "PauseAlgo", "Cleanup" : "SquaredAlgo", "LogCollect" : "SquaredAlgo"}
 
-config.ErrorHandler.maxRetries = {"default" : 30,
-                                  "Cleanup" : 2,
-                                  "LogCollect" : 2}
+config.RetryManager.PauseAlgo.section_('Express')
+config.RetryManager.PauseAlgo.Express.retryErrorCodes = { 70: 0, 50660: 0, 50661: 0, 50664: 0, 71304: 0 }
+config.RetryManager.PauseAlgo.section_('Processing')
+config.RetryManager.PauseAlgo.Processing.retryErrorCodes = { 70: 0, 50660: 0, 50661: 1, 50664: 0, 71304: 1 }
+config.RetryManager.PauseAlgo.section_('Repack')
+config.RetryManager.PauseAlgo.Repack.retryErrorCodes = { 70: 0, 50660: 0, 50661: 0, 50664: 0, 71304: 0 }
+
+config.ErrorHandler.maxRetries = {"default" : 30, "Cleanup" : 2, "LogCollect" : 2}
 
 config.TaskArchiver.useReqMgrForCompletionCheck = False
-
-config.JobArchiver.handleInjected = False
+config.TaskArchiver.dashBoardUrl = "http://dashb-luminosity.cern.ch/dashboard/request.py/putluminositydata"
+config.TaskArchiver.logLevel = "DEBUG"
+config.TaskArchiver.archiveDelayHours = 2190
+config.TaskArchiver.useWorkQueue = False
 
 config.DBS3Upload.primaryDatasetType = "data"
+config.DBS3Upload.datasetType = 'VALID'
 
 config.DBSInterface.primaryDatasetType = "data"
 
 config.AnalyticsDataCollector.pluginName = "Tier0Plugin"
 
-config.JobAccountant.maxAllowedRepackOutputSize = 24 * 1024 * 1024 * 1024
+
+config.AgentStatusWatcher.t1SitesCores = 12.5
+config.AgentStatusWatcher.pendingSlotsTaskPercent = 30
+config.AgentStatusWatcher.pendingSlotsSitePercent = 40
+config.AgentStatusWatcher.runningExpressPercent = 25
+config.AgentStatusWatcher.runningRepackPercent = 10
+config.AgentStatusWatcher.enabled = False
+config.AgentStatusWatcher.onlySSB = False
+
+config.RucioInjector.blockRuleParams = {}
+config.RucioInjector.blockDeletionDelayHours = 168
+config.RucioInjector.useDsetReplicaDeep = True
+
+config.BossAir.pluginNames = ["SimpleCondorPlugin"]
+
