bfd947589741aa3d452073af514821cc99b5ab2f
LinaresToine <antonio.linares.sancho@cern.ch>
2024-05-29 22:37:00 -0500

    Update deployment scripts

--- bin/00_pypi_deploy_replay.sh
+++ bin/00_pypi_deploy_replay.sh
@@ -1,23 +1,65 @@
 #!/bin/bash 
 
+confirm_clearing=""
+echo "This will clear the oracle database, couchdb, and important agent directories"
+sleep 5
+echo "Are you sure you wish to continue? (Y/n)"
+read confirm_clearing
+
+if [ "$confirm_clearing" != "Y" ]
+then
+	echo "Not performing changes"
+    sleep 2
+    echo "Exiting"
+	exit
+fi
+
 BASE_DIR=/data/tier0
-DEPLOY_DIR=/data/tier0/WMAgent.venv3
+DEPLOY_DIR=$BASE_DIR/WMAgent.venv3
+SPEC_DIR=$BASE_DIR/admin/Specs
+
+WMAGENT_TAG=2.3.3
 TIER0_VERSION=3.2.0
+COUCH_TAG=3.2.2
+
 CONFIGURATION_FILE='/data/tier0/admin/ReplayOfflineConfiguration.py'
 WMAGENT_SECRETS=$BASE_DIR/admin/WMAgent.secrets.replay
 CERT=/data/certs/robot-cert-cmst0.pem
 KEY=/data/certs/robot-key-cmst0.pem
 
+echo "Resetting couchdb for new deployment"
+sleep 3
+bash $BASE_DIR/00_reset_couch.sh -t $COUCH_TAG
+
+cd $BASE_DIR
+
+echo "Removing deploy directory"
+sleep 3
+rm -rf $DEPLOY_DIR
+
+echo "Clearing Specs directory"
+sleep 3
+rm -rf $SPEC_DIR
+
+echo "Clearing Oracle Database"
+sleep 3
+bash /data/tier0/00_wipe_t0ast.sh
+
+echo "Installing new wmagent"
+echo "WMAgent version $WMAGENT_TAG"
+sleep 3
+
+./deploy-wmagent-venv.sh -t $WMAGENT_TAG -d $DEPLOY_DIR -y -s
+
+echo "Activating environment"
+sleep 3
 cd $DEPLOY_DIR
 
-echo "Activating virtual environment"
-sleep 3
 source $DEPLOY_DIR/bin/activate
-
-
-echo "Installing T0"
+echo "Installing T0 code"
 sleep 3
 pip install T0==$TIER0_VERSION
+
 
 ###########################
 
