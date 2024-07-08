5e5fc98adf38ec97f75d71ba3b361c3d0ac3df13
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2024-07-08 06:07:48 -0500

    New deployment procedure (#4959)
    * Scripts relevant for deploying the agent in pypi
    
    * Production configuration change to Run2024E and CMSSW_14_0_7 (#4953)
    
    * Update prod config for Era E
    
    * Update etc/ProdOfflineConfiguration.py
    
    Co-authored-by: francescobrivio <francesco.brivio@cern.ch>
    
    * Fix lack of convention in Dataset ProdOfflineConfiguration.py
    
    * Modify timePerEvent of ZeroBias datasets in ProdOfflineConfiguration.py
    
    ---------
    
    Co-authored-by: francescobrivio <francesco.brivio@cern.ch>
    Co-authored-by: Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
    
    * Agent config modifications
    
    * Fix directory naming
    
    * Fixes to new deployment
    
    * Add bin file for simple manipulation of the agent
    
    * Fix syntax issues
    
    * Update clearDeployment function
    
    * init script update
    
    * Deployment scripts updated
    
    * Update deployment scripts
    
    * minor temporary step
    
    * Updated deployment scripts
    
    * Update new deployment scripts
    
    * Changes to override Rucio storage.json (#4956)
    
    * Update __init__.py
    
    * DEployment scripts with taylor made config modifications
    
    * replay configuration for release CMSSW_14_0_7 (#4952)
    
    * Replay Request for 14_0_8 (#4957)
    
    Based on https://github.com/dmwm/T0/pull/4952 except for updated CMSSW release
    
    Co-authored-by: Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
    
    * Update deployment scripts
    
    * addProcessingSite option included
    
    * Adding start agent
    
    * modify resource control procedure
    
    * minor bug fix
    
    * Scripts relevant for deploying the agent in pypi (#4954)
    
    * Scripts relevant for deploying the agent in pypi
    
    * Agent config modifications
    
    * Fix directory naming
    
    * Fixes to new deployment
    
    * Add bin file for simple manipulation of the agent
    
    * Fix syntax issues
    
    * Update clearDeployment function
    
    * init script update
    
    * Deployment scripts updated
    
    * Update deployment scripts
    
    * minor temporary step
    
    * Updated deployment scripts
    
    * Update new deployment scripts
    
    * DEployment scripts with taylor made config modifications
    
    * Update deployment scripts
    
    * addProcessingSite option included
    
    * Adding start agent
    
    * modify resource control procedure
    
    * minor bug fix
    
    ---------
    
    Co-authored-by: Toine <Antonio Linares>
    Co-authored-by: germanfgv <germanfgv@gmail.com>
    
    * Update __init__.py
    
    * Update deployment scripts
    
    * Update t0
    
    ---------
    
    Co-authored-by: Germán Felipe Giraldo Villa <germanfgv@gmail.com>
    Co-authored-by: francescobrivio <francesco.brivio@cern.ch>
    Co-authored-by: jeyserma <janeysermans@gmail.com>
    Co-authored-by: Toine <Antonio Linares>
    Co-authored-by: Giovanni Petrucciani <gpetruc@gmail.com>

--- bin/00_pypi_deploy_replay.sh
+++ bin/00_pypi_deploy_replay.sh
@@ -14,8 +14,9 @@ then
     exit
 fi
 
-WMAGENT_TAG=2.3.4rc4
-TIER0_VERSION=3.2.0
+WMAGENT_TAG=2.3.4rc11
+TIER0_VERSION=3.2.2
+
 COUCH_TAG=3.2.2
 
 BASE_DIR=/data/tier0
@@ -29,19 +30,23 @@ CERT=/data/certs/robot-cert-cmst0.pem
 KEY=/data/certs/robot-key-cmst0.pem
 PROXY=/data/certs/robot-proxy-vocms001.pem
 
+WMA_VENV_DEPLOY_SCRIPT=https://raw.githubusercontent.com/dmwm/WMCore/$WMAGENT_TAG/deploy/deploy-wmagent-venv.sh
 echo "Resetting couchdb for new deployment"
 sleep 3
-bash $BASE_DIR/00_reset_couch.sh -t $COUCH_TAG
+bash $BASE_DIR/00_pypi_reset_couch.sh -t $COUCH_TAG
+
 
 cd $BASE_DIR
 
 echo "Removing deploy directory"
 sleep 3
-rm -rfv $DEPLOY_DIR
+
+rm -rf $DEPLOY_DIR
 
 echo "Clearing Specs directory"
 sleep 3
-rm -rfv $SPEC_DIR/*
+rm -rf $SPEC_DIR/*
+
 
 echo "Clearing Oracle Database"
 sleep 3
@@ -51,7 +56,13 @@ echo "Installing new wmagent"
 echo "WMAgent version $WMAGENT_TAG"
 sleep 3
 
-bash $BASE_DIR/deploy-wmagent-venv.sh -t $WMAGENT_TAG -d $DEPLOY_DIR -y -s
+rm $BASE_DIR/deploy-wmagent-venv.sh
+wget $WMA_VENV_DEPLOY_SCRIPT -O $BASE_DIR/deploy-wmagent-venv.sh
+sed -i 's|\$WMA_CERTS_DIR/myproxy.pem|\$WMA_CERTS_DIR/robot-proxy-vocms001.pem|g' $BASE_DIR/deploy-wmagent-venv.sh
+#bash $BASE_DIR/deploy-wmagent-venv.sh -t $WMAGENT_TAG -d $DEPLOY_DIR -y -s
+
+bash $BASE_DIR/deploy-wmagent-venv.sh -t $WMAGENT_TAG -d $DEPLOY_DIR -y
+
 
 #######################################################################
 echo "Setting up secrets file"
@@ -60,6 +71,9 @@ ln -s $WMAGENT_SECRETS $DEPLOY_DIR/admin/wmagent/WMAgent.secrets
 
 echo "Setting up certificate and key"
 sleep 1
+
+rm $DEPLOY_DIR/certs/*
+
 ln -s $CERT $DEPLOY_DIR/certs/servicecert.pem
 ln -s $KEY $DEPLOY_DIR/certs/servicekey.pem
 ln -s $PROXY $DEPLOY_DIR/certs/myproxy.pem
@@ -72,6 +86,12 @@ source $DEPLOY_DIR/bin/activate
 echo "Installing T0 code"
 sleep 3
 pip install T0==$TIER0_VERSION
+
+chmod +x $DEPLOY_DIR/bin/00*
+chmod +x $DEPLOY_DIR/bin/t0
+
+echo "Applying patches"
+bash $BASE_DIR/00_pypi_patches.sh
 
 echo "Now creating important T0 related environment variables"
 sleep 2
@@ -91,14 +111,6 @@ sleep 1
 echo "variables created successfully"
 sleep 1
 
-#export install=$CURRENT_DIR/install
-#export config=$CURRENT_DIR/config
-#export manage=manage
-
-#echo "activating additional environment variables"
-#source $WMA_ENV_FILE
-
-cp $BASE_DIR/Tier0Config.py $DEPLOY_DIR/etc/Tier0Config.py
 
 ###########################
 
@@ -109,55 +121,58 @@ bash $DEPLOY_DIR/init.sh
 echo "Now populating resource control"
 sleep 3
 
-#Setting for T0_CH_CERN_Disk
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --cms-name=T2_CH_CERN --pnn=T0_CH_CERN_Disk --ce-name=T2_CH_CERN --pending-slots=20000 --running-slots=20000 --plugin=SimpleCondorPlugin
-manage execute-agent wmagent-resource-control --site-name=T0_CH_CERN_Disk --cms-name=T0_CH_CERN_Disk --pnn=T2_CH_CERN --ce-name=T0_CH_CERN_Disk --pending-slots=20000 --running-slots=20000 --plugin=SimpleCondorPlugin
-
-#Setting for T2_CH_CERN
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --cms-name=T2_CH_CERN --pnn=T2_CH_CERN --ce-name=T2_CH_CERN --pending-slots=20000 --running-slots=20000 --plugin=SimpleCondorPlugin
-
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Processing --pending-slots=10000 --running-slots=10000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Merge --pending-slots=1000 --running-slots=1000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Cleanup --pending-slots=1000 --running-slots=1000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=LogCollect --pending-slots=1000 --running-slots=1000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Skim --pending-slots=1 --running-slots=1
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Production --pending-slots=1 --running-slots=1
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Harvesting --pending-slots=1000 --running-slots=1000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Express --pending-slots=3000 --running-slots=3000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Repack --pending-slots=5000 --running-slots=5000
-
-#Settings for using T2_CH_CERN_P5 (PromptReco)
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --cms-name=T2_CH_CERN_P5 --pnn=T2_CH_CERN_P5 --ce-name=T2_CH_CERN_P5 --pending-slots=20000 --running-slots=20000 --plugin=SimpleCondorPlugin
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --task-type=Processing --pending-slots=10000 --running-slots=10000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --task-type=Merge --pending-slots=1000 --running-slots=1000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --task-type=Cleanup --pending-slots=1000 --running-slots=1000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --task-type=LogCollect --pending-slots=1000 --running-slots=1000
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --task-type=Skim --pending-slots=1 --running-slots=1
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --task-type=Production --pending-slots=1 --running-slots=1
-manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN_P5 --task-type=Harvesting --pending-slots=1000 --running-slots=1000
+bash $DEPLOY_DIR/bin/00_pypi_resource_control.sh
 
 echo "Modifying config file"
 sleep 3
+#####
 sed -i 's+TIER0_CONFIG_FILE+'"$CONFIGURATION_FILE"'+' "$config/config.py"
-
-sed -i "s+config.Agent.contact = 'cms-comp-ops-workflow-team@cern.ch'+config.Agent.contact = 'cms-tier0-operations@cern.ch'+" $config/config.py
-sed -i "s+'team1,team2,cmsdataops'+'tier0production'+g" $config/config.py
-
-
-# Twiking Rucio configuration
-sed -i "s+config.RucioInjector.containerDiskRuleParams.*+config.RucioInjector.containerDiskRuleParams = {}+" $config/config.py
-sed -i "s+config.RucioInjector.metaDIDProject.*+config.RucioInjector.metaDIDProject = 'Tier0'+" $config/config.py
-
-sed -i "s+config.DBS3Upload.uploaderName = 'WMAgent'+config.DBS3Upload.uploaderName = 'T0Prod'+g" $config/config.py
+#####
+sed -i "s+config.Agent.teamName = 'REPLACE_TEAM_NAME'+config.Agent.teamName = '"$TEAMNAME"'+" "$config/config.py"
+#####
+sed -i "s+config.Agent.contact = 'cms-comp-ops-workflow-team@cern.ch'+config.Agent.contact = 'cms-tier0-operations@cern.ch'+" "$config/config.py"
+#####
+sed -i "s+'team1,team2,cmsdataops'+'tier0replay'+g" "$config/config.py"
+#####
+sed -i "s+config.RucioInjector.containerDiskRuleParams.*+config.RucioInjector.containerDiskRuleParams = {'lifetime': 7 * 24 * 60 * 60}+" "$config/config.py"
+#####
+echo "config.RucioInjector.blockRuleParams = {'lifetime': 7 * 24 * 60 * 60}" >> "$config/config.py"
+#####
+sed -i "s+config.RucioInjector.metaDIDProject.*+config.RucioInjector.metaDIDProject = 'Test'+" "$config/config.py"
+#####
+echo "config.RucioInjector.blockDeletionDelayHours = 2" >> $config/config.py
+#####
+##### ONLY IN PROD #####
+#echo "config.RucioInjector.blockDeletionDelayHours = 168" >> ./config/tier0/config.py
+#echo 'config.BossAir.pluginNames = ["SimpleCondorPlugin"]' >> ./config/tier0/config.py
+#echo 'config.JobAccountant.maxAllowedRepackOutputSize = 24 * 1024 * 1024 * 1024' >> ./config/tier0/config.py
+#echo "config.AgentStatusWatcher.runningExpressPercent = 25" >> ./config/tier0/config.py
+#echo "config.AgentStatusWatcher.runningRepackPercent = 10" >> ./config/tier0/config.py
+#echo 'config.TaskArchiver.archiveDelayHours = 2190' >> $config.py
+##### ONLY IN PROD #####
+#####
+echo "config.DBS3Upload.datasetType = 'VALID'" >> $config/config.py
+#####
+echo "config.Tier0Feeder.serviceProxy = '$DEPLOY_DIR/certs/myproxy.pem'" >> $config/config.py
+#####
+echo 'config.TaskArchiver.dashBoardUrl = "http://dashb-luminosity.cern.ch/dashboard/request.py/putluminositydata"' >> $config/config.py
+#####
+sed -i "s+config.DBS3Upload.uploaderName = 'WMAgent'+config.DBS3Upload.uploaderName = 'T0Replay'+g" "$config/config.py"
+#####
+#Overwrite ErrorHandler to avoid jobs going to ensure T0 jobs can always be retried
+sed -i "s/config.ErrorHandler.maxFailTime.*/config.ErrorHandler.maxFailTime=601200/g" "$config/config.py"
+#####
+sed -i "s+config.RucioInjector.listTiersToInject.*+config.RucioInjector.listTiersToInject = ['AOD', 'MINIAOD', 'NANOAOD', 'NANOAODSIM', 'RAW', 'FEVT', 'USER', 'ALCARECO', 'ALCAPROMPT','DQMIO','RAW-RECO']+" "$config/config.py"
+#####
+##### ONLY IN REPLAY #####
+sed -i "s/config.RetryManager.plugins.*/config.RetryManager.plugins={'default': 'PauseAlgo', 'Cleanup': 'PauseAlgo', 'LogCollect': 'PauseAlgo'}/g" "$config/config.py"
+sed -i "s/config.ErrorHandler.maxRetries.*/config.ErrorHandler.maxRetries={'default': 30, 'Cleanup': 30, 'LogCollect': 30}/g" "$config/config.py"
+##### ONLY IN REPLAY #####
 
 #
 # Enable AgentStatusWatcher - site status automatic updated
 #
-sed -i "s+config.AgentStatusWatcher.ignoreDisks.*+config.AgentStatusWatcher.ignoreDisks = [ '/cvmfs/cvmfs-config.cern.ch', '/cvmfs/cms.cern.ch', '/eos/cms', '/cvmfs/cms-ib.cern.ch', '/cvmfs/patatrack.cern.ch' ]+" $config/config.py
-
-
-#Overwrite ErrorHandler to avoid jobs going to ensure T0 jobs can always be retried
-sed -i "s/config.ErrorHandler.maxFailTime.*/config.ErrorHandler.maxFailTime=601200/g" $config/config.py
+sed -i "s+config.AgentStatusWatcher.ignoreDisks.*+config.AgentStatusWatcher.ignoreDisks = [ '/cvmfs/cvmfs-config.cern.ch', '/cvmfs/cms.cern.ch', '/eos/cms', '/cvmfs/cms-ib.cern.ch', '/cvmfs/patatrack.cern.ch' ]+" "$config/config.py"
 
 #
 # password for dropbox upload
@@ -180,3 +195,7 @@ echo "You are now in the WMAgent environment"
 
 sleep 1
 echo "Deployment finished"
+
+
+cd $BASE_DIR
+
