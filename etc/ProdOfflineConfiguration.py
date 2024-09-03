66aaf8ef7aca19aa14f782a3c739f72c21b1e452
Germán Felipe Giraldo Villa <germanfgv@gmail.com>
2024-09-03 09:55:21 +0200

    Change to CMSSW_14_0_15

--- etc/ProdOfflineConfiguration.py
+++ etc/ProdOfflineConfiguration.py
@@ -101,7 +101,7 @@ setPromptCalibrationConfig(tier0Config,
 # maxRunPreviousConfig = 999999 # Last run before era change 08/09/23
 # Defaults for CMSSW version
 defaultCMSSWVersion = {
-    'default': "CMSSW_14_0_14",
+    'default': "CMSSW_14_0_15",
     #'acqEra': {'Run2024F': "CMSSW_14_0_11"},
     #'maxRun': {maxRunPreviousConfig: "CMSSW_13_2_2"}
 }
