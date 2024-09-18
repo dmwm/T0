c5ce6c5330b4f6c749f48a508dc4aa38cc71846e
Antonio Linares <a.linares@cern.ch>
2024-09-18 11:57:21 -0500

    Configuration for CMSSW_14_0_16

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -110,7 +110,7 @@ setPromptCalibrationConfig(tier0Config,
 
 # Defaults for CMSSW version
 defaultCMSSWVersion = {
-    'default': "CMSSW_14_0_15"
+    'default': "CMSSW_14_0_16"
 }
 
 # Configure ScramArch
