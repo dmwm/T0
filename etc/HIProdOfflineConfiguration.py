68ef04e09273970e2101d9764736269e25b1203f
Antonio Linares <a.linares@cern.ch>
2024-11-12 11:46:58 -0600

    Era change HIRun2024B

--- etc/HIProdOfflineConfiguration.py
+++ etc/HIProdOfflineConfiguration.py
@@ -60,7 +60,7 @@ addSiteConfig(tier0Config, "T0_CH_CERN_Disk",
 #  Data type
 #  Processing site (where jobs run)
 #  PhEDEx locations
-setAcquisitionEra(tier0Config, "HIRun2024A")
+setAcquisitionEra(tier0Config, "HIRun2024B")
 setEmulationAcquisitionEra(tier0Config, "Emulation2024")
 setBaseRequestPriority(tier0Config, 251000)
 setBackfill(tier0Config, None)
