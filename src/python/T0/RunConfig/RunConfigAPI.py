1db87d7f700744724d811ff42f1ebc83ad9ced6e
Antonio Linares <alinares@ggiraldodesktop.dyndns.cern.ch>
2025-01-21 16:53:50 +0100

    Update RunConfigAPI

--- src/python/T0/RunConfig/RunConfigAPI.py
+++ src/python/T0/RunConfig/RunConfigAPI.py
@@ -858,6 +858,7 @@ def releasePromptReco(tier0Config, specDirectory, dqmUploadProxy):
             if len(datasetConfig.DqmSequences) > 0:
                 dqmSeq = ",".join(datasetConfig.DqmSequences)
 
+            nanoFlavours = None
             if len(datasetConfig.NanoFlavours) > 0:
                 nanoFlavours = ",".join(datasetConfig.NanoFlavours)
 
