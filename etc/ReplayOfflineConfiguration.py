a7b59a9019ae39b6648ba10f8d1ee288b78a01d9
Antonio <alinares@alinaresdesktop.dyndns.cern.ch>
2025-03-24 18:07:21 +0100

    Addition of CosmicHLTMonitor Express PD

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -330,6 +330,31 @@ addExpressConfig(tier0Config, "HLTMonitor",
                  dataset_lifetime=replayDatasetLifetime,#lifetime for container rules. Default 14 days
                  versionOverride=expressVersionOverride)
 
+addExpressConfig(tier0Config, "CosmicHLTMonitor",
+                 scenario=cosmicsScenario,
+                 diskNode="T0_CH_CERN_Disk",
+                 data_tiers=["FEVTHLTALL"],
+                 write_dqm=True,
+                 alca_producers=[],
+                 dqm_sequences=["@HLTMon"],
+                 reco_version=defaultCMSSWVersion,
+                 multicore=numberOfCores,
+                 global_tag_connect=globalTagConnect,
+                 global_tag=expressGlobalTag,
+                 proc_ver=expressProcVersion,
+                 maxInputRate=23 * 1000,
+                 maxInputEvents=400,
+                 maxInputSize=2 * 1024 * 1024 * 1024,
+                 maxInputFiles=15,
+                 maxLatency=15 * 23,
+                 periodicHarvestInterval=20 * 60,
+                 blockCloseDelay=1200,
+                 timePerEvent=4, #I have to get some stats to set this properly
+                 sizePerEvent=1700, #I have to get some stats to set this properly
+                 maxMemoryperCore=2000,
+                 dataset_lifetime=replayDatasetLifetime,#lifetime for container rules. Default 14 days
+                 versionOverride=expressVersionOverride)
+
 addExpressConfig(tier0Config, "Calibration",
                  scenario=alcaTestEnableScenario,
                  data_tiers=["RAW"],
