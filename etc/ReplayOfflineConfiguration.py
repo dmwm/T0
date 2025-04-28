b64ec79ef1528a3084abe14a2a12b4447f78e894
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2025-04-28 11:17:05 +0200

    Update etc/ReplayOfflineConfiguration.py
    Co-authored-by: Marino Missiroli <m.missiroli@cern.ch>

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -820,7 +820,7 @@ for dataset in DATASETS:
                physics_skims=["ZMu", "LogError", "LogErrorMonitor"],
                scenario=ppScenario)
 
-DATASETS = ["EGamma", "EGamma0", "EGamma1", "EGamma2", "Egamma3"]
+DATASETS = ["EGamma", "EGamma0", "EGamma1", "EGamma2", "EGamma3"]
 
 for dataset in DATASETS:
     addDataset(tier0Config, dataset,
