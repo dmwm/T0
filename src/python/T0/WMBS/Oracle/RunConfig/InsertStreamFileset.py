f09e3873932b6e3022d7f1c2d06fad55f9dd2bd4
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-05-31 15:57:28 -0400

    Replace Oracle SEQUENCE constraints used by GENERATE auto-increment

--- src/python/T0/WMBS/Oracle/RunConfig/InsertStreamFileset.py
+++ src/python/T0/WMBS/Oracle/RunConfig/InsertStreamFileset.py
@@ -14,11 +14,11 @@ class InsertStreamFileset(DBFormatter):
 
         sql = """INSERT ALL
                    INTO wmbs_fileset
-                     (ID, NAME, LAST_UPDATE, OPEN)
-                     VALUES (wmbs_fileset_SEQ.nextval, :NAME, :TIME, 1)
+                     (NAME, LAST_UPDATE, OPEN)
+                     VALUES (:NAME, :TIME, 1)
                    INTO run_stream_fileset_assoc
-                     (RUN_ID, STREAM_ID, FILESET)
-                     VALUES (:RUN, stream_id, wmbs_fileset_SEQ.nextval)
+                     (RUN_ID, STREAM_ID)
+                     VALUES (:RUN, stream_id)
                  SELECT id AS stream_id
                  FROM stream
                  WHERE name = :STREAM
