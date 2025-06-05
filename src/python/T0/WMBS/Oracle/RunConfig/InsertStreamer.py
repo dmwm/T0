7808d85ed96a879ab8e493d812d2e807c6e2706b
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-06-05 11:16:08 -0400

    Fix INSERT ALL in InsertStreamer

--- src/python/T0/WMBS/Oracle/RunConfig/InsertStreamer.py
+++ src/python/T0/WMBS/Oracle/RunConfig/InsertStreamer.py
@@ -13,31 +13,37 @@ class InsertStreamer(DBFormatter):
 
         sql = """DECLARE
                    cnt NUMBER(1);
+                   file_id NUMBER;
+                   stream_id NUMBER;
                  BEGIN
                    SELECT COUNT(*)
                    INTO cnt
                    FROM wmbs_file_details
                    WHERE lfn = :LFN
                    ;
+
+                   SELECT id INTO stream_id
+                   FROM stream
+                   WHERE name = :STREAM;
+
                    IF (cnt = 0)
                    THEN
-                     INSERT ALL
-                       INTO wmbs_file_details
-                         (LFN, FILESIZE, EVENTS, MERGED)
-                         VALUES (:LFN, :FILESIZE, :EVENTS, '0')
-                       INTO wmbs_file_runlumi_map
-                         (RUN, LUMI)
-                         VALUES (:RUN, :LUMI)
-                       INTO wmbs_file_location
-                         (PNN)
-                         VALUES ((SELECT id FROM wmbs_pnns WHERE pnn = '%s'))
-                       INTO streamer
-                         (P5_ID, RUN_ID, STREAM_ID, LUMI_ID, INSERT_TIME)
-                         VALUES (:P5_ID, :RUN, stream_id, :LUMI, :TIME)
-                     SELECT id AS stream_id
-                     FROM stream
-                     WHERE name = :STREAM
-                     ;
+                     INSERT INTO wmbs_file_details
+                       (LFN, FILESIZE, EVENTS, MERGED)
+                       VALUES (:LFN, :FILESIZE, :EVENTS, '0')
+                       RETURNING id INTO file_id;
+
+                     INSERT INTO wmbs_file_runlumi_map
+                       (FILEID, RUN, LUMI)
+                       VALUES (file_id, :RUN, :LUMI);
+
+                     INSERT INTO wmbs_file_location
+                       (FILEID, PNN)
+                       VALUES (file_id, (SELECT id FROM wmbs_pnns WHERE pnn = '%s'));
+
+                     INSERT INTO streamer
+                       (ID, P5_ID, RUN_ID, STREAM_ID, LUMI_ID, INSERT_TIME)
+                       VALUES (file_id, :P5_ID, :RUN, stream_id, :LUMI, :TIME);
                    END IF;
                  EXCEPTION
                    WHEN DUP_VAL_ON_INDEX THEN NULL;
