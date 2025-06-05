8e50d353432927ea659baa0c80cb84189e4e9a13
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-06-05 11:20:26 -0400

    Fix INSERT ALL for InsertStreamFileset

--- src/python/T0/WMBS/Oracle/RunConfig/InsertStreamFileset.py
+++ src/python/T0/WMBS/Oracle/RunConfig/InsertStreamFileset.py
@@ -12,16 +12,23 @@ class InsertStreamFileset(DBFormatter):
 
     def execute(self, run, stream, name, conn = None, transaction = False):
 
-        sql = """INSERT ALL
-                   INTO wmbs_fileset
+        sql = """DECLARE
+                   fileset_id NUMBER;
+                   stream_id NUMBER;
+                 BEGIN
+                   SELECT id INTO stream_id
+                   FROM stream
+                   WHERE name = :STREAM;
+
+                   INSERT INTO wmbs_fileset
                      (NAME, LAST_UPDATE, OPEN)
                      VALUES (:NAME, :TIME, 1)
-                   INTO run_stream_fileset_assoc
-                     (RUN_ID, STREAM_ID)
-                     VALUES (:RUN, stream_id)
-                 SELECT id AS stream_id
-                 FROM stream
-                 WHERE name = :STREAM
+                     RETURNING id INTO fileset_id;
+
+                   INSERT INTO run_stream_fileset_assoc
+                     (RUN_ID, STREAM_ID, FILESET)
+                     VALUES (:RUN, stream_id, fileset_id);
+                 END;
                  """
 
         binds = { 'RUN' : run,
