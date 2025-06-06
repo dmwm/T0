a5a92ee5bfe5b40f16a0b514ad04ec09b9c057c3
Antonio Linares <a.linares@cern.ch>
2025-06-06 11:58:34 +0200

    era change Run2025D

--- etc/ProdOfflineConfiguration.py
+++ etc/ProdOfflineConfiguration.py
@@ -60,7 +60,7 @@ addSiteConfig(tier0Config, "T0_CH_CERN_Disk",
 #  Data type
 #  Processing site (where jobs run)
 #  PhEDEx locations
-setAcquisitionEra(tier0Config, "Run2025C")
+setAcquisitionEra(tier0Config, "Run2025D")
 setEmulationAcquisitionEra(tier0Config, "Emulation2025")
 setBaseRequestPriority(tier0Config, 251000)
 setBackfill(tier0Config, None)
