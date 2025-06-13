a18af6fc9cc60bc3251a09703bb8febd89e26e7e
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2025-06-13 12:26:50 +0200

    Fix scenario in Create.py

--- src/python/T0/WMBS/Oracle/Create.py
+++ src/python/T0/WMBS/Oracle/Create.py
@@ -952,7 +952,7 @@ class Create(DBCreator):
                            58 : "ppEra_Run3_2025",
                            59 : "AlCaHcalIsoTrk_Run3",
                            60 : "hltScoutingEra_Run3_2025",
-                           61 : "Run3_2025_OXY"
+                           61 : "ppEra_Run3_2025_OXY"
                          }
         for id, name in list(eventScenarios.items()):
             sql = """INSERT INTO event_scenario
