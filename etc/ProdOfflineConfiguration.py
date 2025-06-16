5cf3ff291b6d8535ec228d3432772847ed501f14
Viphava280444 <87804447+Viphava280444@users.noreply.github.com>
2025-06-16 11:50:10 +0200

    Change version of production to CMSSW_15_0_8

--- etc/ProdOfflineConfiguration.py
+++ etc/ProdOfflineConfiguration.py
@@ -101,9 +101,9 @@ setPromptCalibrationConfig(tier0Config,
 maxRunPreviousConfig = 392513 # Last run before era change 08/09/23
 # Defaults for CMSSW version
 defaultCMSSWVersion = {
-    'default': "CMSSW_15_0_7",
+    'default': "CMSSW_15_0_8",
     #'acqEra': {'Run2024F': "CMSSW_14_0_11"},
-    'maxRun': {maxRunPreviousConfig: "CMSSW_15_0_6"}
+    #'maxRun': {maxRunPreviousConfig: "CMSSW_15_0_6"}
 }
 
 # Configure ScramArch
