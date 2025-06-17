2cca3556d7413e298d69b657cc37cc380ce75a48
Antonio <alinares@alinaresdesktop.dyndns.cern.ch>
2025-06-17 17:06:53 +0200

    COnfig file for buggy replay

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -146,6 +146,8 @@ hiTestppScenario = "ppEra_Run3_pp_on_PbPb_2023"
 hiRawPrimeScenario = "ppEra_Run3_pp_on_PbPb_approxSiStripClusters_2023"
 hltScoutingScenario = "hltScoutingEra_Run3_2025"
 AlCaHcalIsoTrkScenario = "AlCaHcalIsoTrk_Run3"
+OXYScenario = "ppEra_Run3_2025_OXY"
+
 
 # Procesing version number replays
 # Taking Replay processing ID from the last 8 digits of the DeploymentID
@@ -157,7 +159,7 @@ alcarawProcVersion = dt
 # Defaults for GlobalTag
 
 
-expressGlobalTag = "150X_dataRun3_Express_v1"
+expressGlobalTag = "150X_dataRun3_Express_v2"
 promptrecoGlobalTag = "150X_dataRun3_Prompt_v1"
 repackGlobalTag = "150X_dataRun3_Prompt_v1_ParkingDoubleMuonLowMass_v2"
 
@@ -280,7 +282,7 @@ addExpressConfig(tier0Config, "Express",
                  blockCloseDelay=1200,
                  timePerEvent=4,
                  sizePerEvent=1700,
-                 maxMemoryperCore=2000,
+                 maxMemoryperCore=100,
                  dataset_lifetime=7*24*3600,#lifetime for container rules. Default 14 days
                  versionOverride=expressVersionOverride)
 
@@ -608,9 +610,9 @@ for dataset in DATASETS:
     addDataset(tier0Config, dataset,
                do_reco=True,
                write_dqm=True,
-               dqm_sequences=["@common", "@muon", "@heavyFlavor"],
+               dqm_sequences=["@common", "@mun", "@heavyFlavor"],
                alca_producers=["TkAlJpsiMuMu", "TkAlUpsilonMuMu"],
-               nano_flavours=["@PHYS", "@L1", "@BPH"],
+               nano_flavours=["@PHYS", "@L1", "@B"],
                tape_node=None,
                scenario=ppScenario)
 
