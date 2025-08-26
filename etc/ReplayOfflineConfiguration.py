56e70b545346e1fc6316f55a6fe04a28e9c67ff4
Antonio <alinares@alinaresdesktop.dyndns.cern.ch>
2025-08-26 14:01:52 +0200

    CMSSW_15_0_13_patch1

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -147,7 +147,7 @@ setPromptCalibrationConfig(tier0Config,
 
 # Defaults for CMSSW version
 defaultCMSSWVersion = {
-    'default': "CMSSW_15_0_13"
+    'default': "CMSSW_15_0_13_patch1"
 }
 
 # Configure ScramArch
