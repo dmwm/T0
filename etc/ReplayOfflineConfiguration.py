71114514a1bd692ef58c6ca7a45bf4f27cd2126b
LinaresToine <antonio.linares.sancho@cern.ch>
2026-02-23 11:42:17 +0100

    include scenario 2026

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -170,7 +170,7 @@ setScramArch(tier0Config, "CMSSW_12_3_0", "cs8_amd64_gcc10")
 setScramArch(tier0Config, "CMSSW_13_0_9", "el8_amd64_gcc11")
 
 # Configure scenarios
-ppScenario = "ppEra_Run3_2025"
+ppScenario = "ppEra_Run3_2026"
 ppRefScenario = "ppEra_Run3_2024_ppRef"
 ppScenarioB0T = "ppEra_Run3"
 cosmicsScenario = "cosmicsEra_Run3"
